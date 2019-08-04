9e8d9f29f1b8802599d92fdc7dc1624725da569e
Sri Hari S <46963593+srhr17@users.noreply.github.com>
2019-08-04 19:11:28 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ import Home from "./compnents/home.jsx";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import Ownerlogin from "./compnents/ownerlogin.jsx";
-ReactDOM.render(<Home />, document.getElementById("root"));
+import Customerlogin from "./compnents/customerlogin.jsx";
+import Adminlogin from "./compnents/adminlogin.jsx";
+import Signup from "./compnents/signup.jsx";
+ReactDOM.render(<Signup />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
